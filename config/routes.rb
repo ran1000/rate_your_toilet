9525123c4934fa19578f1e9f6a4eb6cc653fe592
Florian Bartel <bartel.florian@live.de>
2022-05-31 13:15:43 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :toilets, only: %i[new create edit update show index destroy] do
+    collection do
+      get :golden
+    end
+    resources :reviews, only: %i[new create show index destroy]
+    resources :favorites, only: %i[new create index destroy]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

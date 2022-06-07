e7cbdb2c4b20f048808cf132495bc0e69c2cd2e4
Sebastian Landucci <sebastianlanducci@gmail.com>
2022-06-07 14:23:23 +0200

    map max distance filter

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -6,7 +6,7 @@ class ToiletsController < ApplicationController
 
     # @reviews = Review.where(urinal: true)
     # @toilets = Toilet.where(id: toilet_id)
-    @toilets = policy_scope(Toilet.all.near([params[:lat], params[:lng]], 1, units: :km))
+    @toilets = policy_scope(Toilet.all.near([params[:lat], params[:lng]], 5, units: :km))
 
     # @toilets = policy_scope(Toilet.reviews.where(urinal: true))
     @markers = @toilets.geocoded.map do |toilet|

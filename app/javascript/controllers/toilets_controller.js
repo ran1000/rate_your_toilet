0e87c372cb2460645999db2802109eabd909fd5c
Andrea Spiga <drespriz@gmail.com>
2022-06-08 18:06:52 +0200

    toggle on off map and cards in index

--- app/javascript/controllers/toilets_controller.js
+++ app/javascript/controllers/toilets_controller.js
@@ -1,13 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["btn", "map", "toilets"]
+  static targets = ["btn", "map", "toilets", "close"]
 
   connect() {
-    console.log(this.btnTarget)
+    // console.log(this.btnTarget)
   }
   open() {
     this.toiletsTarget.classList.remove("d-none")
     this.mapTarget.classList.add("d-none")
+  }
+  close() {
+    this.toiletsTarget.classList.add("d-none")
+    this.mapTarget.classList.remove("d-none")
   }
 }

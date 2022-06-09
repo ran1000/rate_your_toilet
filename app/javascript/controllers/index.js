0efcd46405585243e1cbee679c03a66c96e7b8a8
Sebastian Landucci <sebastianlanducci@gmail.com>
2022-06-09 17:45:06 +0200

    autoccomplete

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AddressAutocompleteController from "./address_autocomplete_controller.js"
+application.register("address-autocomplete", AddressAutocompleteController)
+
 import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
 

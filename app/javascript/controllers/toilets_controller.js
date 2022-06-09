4cc1f6d7e133524c3e328ec3baeecf6dc1d002ec
Andrea Spiga <drespriz@gmail.com>
2022-06-09 09:05:51 +0200

    fixes filters bars d-none when show all toilets cards

--- app/javascript/controllers/toilets_controller.js
+++ app/javascript/controllers/toilets_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["btn", "map", "toilets", "close"]
+  static targets = ["btn", "map", "toilets", "close", "filters"]
 
   connect() {
     // console.log(this.btnTarget)
@@ -9,9 +9,11 @@ export default class extends Controller {
   open() {
     this.toiletsTarget.classList.remove("d-none")
     this.mapTarget.classList.add("d-none")
+    this.filtersTarget.classList.add("d-none")
   }
   close() {
     this.toiletsTarget.classList.add("d-none")
+    this.filtersTarget.classList.remove("d-none")
     this.mapTarget.classList.remove("d-none")
   }
 }
